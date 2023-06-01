b12676b5ec7ba0c307bc2e948d5845a39026ac83
Dmytro_Golysh <disist357@gmail.com>
2023-06-01 22:15:14 +0300

    Add detailed coverage comment.

--- merge-coverage-json.js
+++ merge-coverage-json.js
@@ -1,14 +1,16 @@
 const coverageFolder = './coverage/apps/';
 const fs = require('fs');
 
-let agregatedCoverage = {};
-fs.readdirSync(coverageFolder).forEach((appName) => {
-  const appCoverage = require(`${coverageFolder}${appName}/coverage-summary.json`);
-  agregatedCoverage = { ...agregatedCoverage, ...appCoverage };
-});
+['coverage-summary.json', 'coverage-final.json'].forEach((coverageFile) => {
+  let agregatedCoverage = {};
+  fs.readdirSync(coverageFolder).forEach((appName) => {
+    const appCoverage = require(`${coverageFolder}${appName}/${coverageFile}`);
+    agregatedCoverage = { ...agregatedCoverage, ...appCoverage };
+  });
 
-// console.log(JSON.stringify(agregatedCoverage, null, 2));
-fs.writeFileSync(
-  './coverage/coverage-summary.json',
-  JSON.stringify(agregatedCoverage)
-);
+  // console.log(JSON.stringify(agregatedCoverage, null, 2));
+  fs.writeFileSync(
+    `./coverage/${coverageFile}`,
+    JSON.stringify(agregatedCoverage)
+  );
+});
