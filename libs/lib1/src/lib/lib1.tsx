63cc52ae5d7cca453bea4b3e5d52eaf9fbcdce79
Dmitry Golysh <disist@users.noreply.github.com>
2023-06-01 23:16:01 +0300

    Add untested code (#5)
    * Add unteste code
    
    * Show coverage comment
    
    * Add detailed coverage comment.
    
    * Add istambul
    
    * Echo coverage.txt
    
    * Text nyc

--- libs/lib1/src/lib/lib1.tsx
+++ libs/lib1/src/lib/lib1.tsx
@@ -1,16 +1,28 @@
+import { useMemo } from 'react';
 import styled from 'styled-components';
 
 /* eslint-disable-next-line */
-export interface Lib1Props {}
+export interface Lib1Props {
+  someVariable?: boolean;
+}
 
 const StyledLib1 = styled.div`
   color: pink;
 `;
 
 export function Lib1(props: Lib1Props) {
+  const anyConditionalValue = useMemo(() => {
+    if (props.someVariable) {
+      return 1000;
+    }
+
+    return 2000;
+  }, [props.someVariable]);
+
   return (
     <StyledLib1>
       <h1>Welcome to Lib1!</h1>
+      <div>{anyConditionalValue}</div>
     </StyledLib1>
   );
 }
